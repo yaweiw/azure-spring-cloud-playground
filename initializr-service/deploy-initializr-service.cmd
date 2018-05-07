4d7e0064f2a4ab03a290b6558cdc09353451038a
Pan Li <panli@microsoft.com>
2018-05-07 11:25:46 +0800

    Add placeholders to pom.xml build plugin configuration.
    * Be more friendly to use side.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- initializr-service/deploy-initializr-service.cmd
+++ initializr-service/deploy-initializr-service.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 
 cd ../ && mvn clean package -Dmaven.test.skip=true -Pfull && cd initializr-service
-docker push plincar.azurecr.io/azure-spring-cloud:latest && mvn azure-webapp:deploy
+docker push ${your-acr-username}/azure-spring-cloud:latest && mvn azure-webapp:deploy
--- initializr-service/deploy-initializr-service.sh
+++ initializr-service/deploy-initializr-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ../ && mvn clean package -Dmaven.test.skip=true -Pfull && cd -
-docker push plincar.azurecr.io/azure-spring-cloud:latest && mvn azure-webapp:deploy
+docker push ${your-acr-username}/azure-spring-cloud:latest && mvn azure-webapp:deploy
 
